431a81a291db41c99df5f3978a0d4ef9ff0369a7
Dmitriy <dfajl@mail.ru>
2023-11-15 10:23:27 +0300

    solve MarkedList

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,32 +1,62 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// From https://jsonplaceholder.typicode.com/comments
-const emails = [
-  'Eliseo@gardner.biz',
-  'Jayne_Kuhic@sydney.com',
-  'Nikita@garfield.biz',
-  'Lew@alysha.tv',
-  'Hayden@althea.biz',
-  'Presley.Mueller@myrl.com',
-  'Dallas@ole.me',
-  'Mallory_Kunze@marie.org',
-  'Meghan_Littel@rene.us',
-  'Carmen_Keeling@caroline.name',
-  'Veronica_Goodwin@timmothy.net',
-  'Oswald.Vandervort@leanne.org',
-  'Kariane@jadyn.tv',
-  'Nathan@solon.io',
-  'Maynard.Hodkiewicz@roberta.com',
-  'Christine@ayana.info',
-  'Preston_Hudson@blaise.tv',
-  'Vincenza_Klocko@albertha.name',
-  'Madelynn.Gorczany@darion.biz',
-  'Mariana_Orn@preston.org',
-  'Noemie@marques.me',
-  'Khalil@emile.co.uk',
-  'Sophia@arianna.co.uk',
-  'Jeffery@juwan.us',
-  'Isaias_Kuhic@jarrett.net',
-];
+const App = defineComponent({
+  name: 'App',
 
-// Требуется создать Vue приложение
+  data() {
+    return {
+      emails: [
+        'Eliseo@gardner.biz',
+        'Jayne_Kuhic@sydney.com',
+        'Nikita@garfield.biz',
+        'Lew@alysha.tv',
+        'Hayden@althea.biz',
+        'Presley.Mueller@myrl.com',
+        'Dallas@ole.me',
+        'Mallory_Kunze@marie.org',
+        'Meghan_Littel@rene.us',
+        'Carmen_Keeling@caroline.name',
+        'Veronica_Goodwin@timmothy.net',
+        'Oswald.Vandervort@leanne.org',
+        'Kariane@jadyn.tv',
+        'Nathan@solon.io',
+        'Maynard.Hodkiewicz@roberta.com',
+        'Christine@ayana.info',
+        'Preston_Hudson@blaise.tv',
+        'Vincenza_Klocko@albertha.name',
+        'Madelynn.Gorczany@darion.biz',
+        'Mariana_Orn@preston.org',
+        'Noemie@marques.me',
+        'Khalil@emile.co.uk',
+        'Sophia@arianna.co.uk',
+        'Jeffery@juwan.us',
+        'Isaias_Kuhic@jarrett.net',
+      ],
+      searchValue: '',
+    };
+  },
+  computed: {
+    /* filteredEmails() {
+      this.$refs.li.forEach((li) => {
+        if (li.innerText.toLowerCase().includes(this.searchValue) && this.searchValue) {
+          li.classList.add('marked');
+        } else {
+          li.classList.remove('marked');
+        }
+      });
+    }, */
+  },
+  watch: {
+    searchValue(newValue) {
+      this.$refs.li.forEach((li) => {
+        if (li.innerText.toLowerCase().includes(newValue) && newValue) {
+          li.classList.add('marked');
+        } else {
+          li.classList.remove('marked');
+        }
+      });
+    },
+  },
+});
+
+const vm = createApp(App).mount('#app');
