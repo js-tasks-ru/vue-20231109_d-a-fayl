12f4a4470479e1004ee07db1908a5d81496f7daa
Dmitriy <dfajl@mail.ru>
2023-11-19 11:17:16 +0300

    redo 04-MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,30 +1,30 @@
 import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
+
+function fetchMeetupById(meetupId) {
+  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      return response.json().then((error) => {
+        throw error;
+      });
+    }
+  });
+}
+
 const App = defineComponent({
   name: 'App',
   data() {
     return {
       inputValue: '',
-      MeetupTitle: '',
+      meetupTitle: '',
     };
-  },
-  methods: {
-    async fetchMeetupByID(meetupId) {
-      try {
-        const response = await fetch(`${API_URL}/meetups/${meetupId}`);
-        if (response.ok) {
-          const result = await response.json();
-          this.MeetupTitle = result.title;
-        }
-      } catch (error) {
-        alert(error);
-      }
-    },
   },
   watch: {
     inputValue(newValue) {
-      this.fetchMeetupByID(newValue);
+      fetchMeetupById(newValue).then((meetup) => (this.meetupTitle = meetup.title));
     },
   },
 });
