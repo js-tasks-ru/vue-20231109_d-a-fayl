f03d36b3856f07bb3e32e7970dcb52730a80f113
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-11-25 10:23:30 +0300

    Added solution for task: MarkedList

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
 const emails = [
@@ -29,4 +29,20 @@ const emails = [
   'Isaias_Kuhic@jarrett.net',
 ];
 
-// Требуется создать Vue приложение
+createApp({
+  data() {
+    return {
+      emails,
+      filter: '',
+    };
+  },
+
+  computed: {
+    markedEmails() {
+      return this.emails.map((email) => ({
+        email,
+        marked: this.filter && email.includes(this.filter),
+      }));
+    },
+  },
+}).mount('#app');
