df722497913d015ebac53db17ac29c534e264113
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-11-27 15:40:06 +0300

    Added solution for task: Компонент CounterButton

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -3,7 +3,24 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'CounterButton',
 
-  // Компонент должен иметь входной параметр и порождать событие
+  // Теперь текущее значение счётчика приходит от родителя через входной параметр
+  props: {
+    count: {
+      // Не забываем указать тип
+      type: Number,
+      // Значение по умолчанию - 0
+      default: 0,
+    },
+  },
 
-  template: `<button type="button">1</button>`,
+  emits: ['update:count'],
+
+  methods: {
+    increment() {
+      // При клике на кнопку порождаем событие и отправляем новое значение
+      this.$emit('update:count', this.count + 1);
+    },
+  },
+
+  template: `<button type="button" @click="increment">{{ count }}</button>`,
 });
