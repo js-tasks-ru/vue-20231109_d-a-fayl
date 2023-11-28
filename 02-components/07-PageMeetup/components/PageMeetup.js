42dcbd3b5c832ff7a3ea1d5ebfb4041110dae709
Dmitriy <dfajl@mail.ru>
2023-11-28 16:19:38 +0300

    solve 07-PageMeetup

--- 02-components/07-PageMeetup/components/PageMeetup.js
+++ 02-components/07-PageMeetup/components/PageMeetup.js
@@ -1,26 +1,76 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
+import { fetchMeetupById } from '../meetupService.js';
+
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import MeetupDescription from '../../02-MeetupDescription/components/MeetupDescription';
+import MeetupCover from '../../03-MeetupCover/components/MeetupCover.js';
+import MeetupInfo from '../../04-MeetupInfo/components/MeetupInfo.js';
+import MeetupAgendaItem from '../../05-MeetupAgenda/components/MeetupAgendaItem.js';
+import MeetupAgenda from '../../05-MeetupAgenda/components/MeetupAgenda';
+import MeetupView from '../../06-MeetupView/components/MeetupView';
 
 export default defineComponent({
   name: 'PageMeetup',
-
+  props: {
+    meetupId: {
+      type: [Number],
+      required: true,
+    },
+  },
   components: {
     UiAlert,
     UiContainer,
+    MeetupDescription,
+    MeetupCover,
+    MeetupInfo,
+    MeetupAgendaItem,
+    MeetupAgenda,
+    MeetupView,
+  },
+  data() {
+    return {
+      meetup: null,
+      errorMessage: null,
+      isLoading: false,
+    };
+  },
+  mounted() {
+    this.isLoading = true;
+    fetchMeetupById(this.meetupId)
+      .then((meetup) => {
+        this.meetup = meetup;
+      })
+      .catch((error) => {
+        this.errorMessage = error.message;
+      })
+      .finally(() => (this.isLoading = false));
+  },
+  watch: {
+    meetupId(newID) {
+      this.isLoading = true;
+      fetchMeetupById(newID)
+        .then((meetup) => {
+          this.meetup = meetup;
+        })
+        .catch((error) => {
+          this.meetup = null;
+          this.errorMessage = error.message;
+        })
+        .finally(() => (this.isLoading = false));
+    },
   },
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
-
-      <UiContainer>
+      <UiContainer v-if="isLoading">
         <UiAlert>Загрузка...</UiAlert>
       </UiContainer>
-
-      <UiContainer>
-        <UiAlert>error</UiAlert>
-      </UiContainer>
+      <template v-else>
+        <MeetupView v-if="meetup" :meetup="meetup" />
+        <UiContainer v-else>
+          <UiAlert> {{errorMessage}} </UiAlert>
+        </UiContainer>
+      </template>
     </div>`,
 });
